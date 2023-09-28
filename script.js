bb9cde63dfcff7840d005794914de8e28d6bf68d
yigitEnes <96189534+ubik69@users.noreply.github.com>
2023-09-28 10:52:21 +0100

    added few stuff
    added javascript to find us page.
    added button to trigger javascript that gets a random audio each time.
    
    tbd = 
    add it to every pages.
    create a custom animation that simulates the button we press in bitten.

---
+++ script.js
@@ -0,0 +1,20 @@
+function randomAudio(){/*script to get a random audio on click every time.*/
+let floattedRandom = Math.random()*5;               
+let randInt = Math.floor(floattedRandom);
+if (randInt == 1) {
+    audio1.play()
+}
+else if(randInt == 2){
+    audio2.play()
+}
+else if(randInt == 3){
+    audio3.play()
+}
+else if(randInt == 4){
+    audio4.play()
+}
+else{
+    audio5.play()
+}
+}
+randomAudio();
