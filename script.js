2e41888bab262b300b538b69587471fa234a77bf
yigitEnes <96189534+ubik69@users.noreply.github.com>
2023-10-09 12:28:00 +0100

    big chunk of order processing is completed.
    To be able to get selections from one page to another is really challenging if you're not using any serverside scripts however , there are futures built-in browsers such as Local Storage , Session Storage or Cookies to be able to direct values. Since we're not using back-end in this project , I choosed to use localStorage because it has most amount of data capacity to hold and easy to clean afterwards. But of course using back-end is always the safest option.

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ function changeText(){
     setTimeout(direction,1000)
     let result = document.getElementById("result");
     result.innerHTML = '<br> Wait till we direct you to the checkout page.';
+        // this is where it should collect the values of selections.
+    selectElement = document.querySelector('#selected_burger');
+    selectElement2 = document.querySelector('#selected_fries')
+    selectElement3 = document.querySelector('#selected_milkshake')
+    output = selectElement.value;
+    localStorage.setItem("burger_Selected", output);
+    output2= selectElement2.value;
+    localStorage.setItem("fries_Selected", output2);
+    output3= selectElement3.value;
+    localStorage.setItem("drink_Selected", output3);
+    alert(output);
+    alert(output2);
+    alert(output3);
 }
 
+// reason to use multiple functions is that setTimeout function executes the codes in that function and after that it sleeps for entered amount. I wanted it to wait for audios to fully play and then direct to the page to enhance UX.
 
 function direction(){
     window.location.href='checkout.html';
