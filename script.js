686fb8a482a369c46bb6f5d81110ec5061ec8002
yigitEnes <96189534+ubik69@users.noreply.github.com>
2023-10-10 23:58:50 +0100

    added validations to javascript
    self explanatory , checks if needed pieces were chosen or not and gives error if needed.

--- script.js
+++ script.js
@@ -11,21 +11,26 @@ function changeText(){
     selectElement = document.querySelector('#selected_burger');
     selectElement2 = document.querySelector('#selected_fries')
     selectElement3 = document.querySelector('#selected_milkshake')
+    if (selectElement.value == "Select your burger choice" || selectElement2.value=="Select your side"){ // checks before assigning numbers to checkout page.
+        alert("Make sure to select atleast one burger and side to proceed!");
+    }
     output = selectElement.value;
     localStorage.setItem("burger_Selected", output);
     output2= selectElement2.value;
     localStorage.setItem("fries_Selected", output2);
     output3= selectElement3.value;
     localStorage.setItem("drink_Selected", output3);
-    alert(output);
-    alert(output2);
-    alert(output3);
 }
 
 // reason to use multiple functions is that setTimeout function executes the codes in that function and after that it sleeps for entered amount. I wanted it to wait for audios to fully play and then direct to the page to enhance UX.
 
 function direction(){
-    window.location.href='checkout.html';
+    if (selectElement.value == "Select your burger choice" || selectElement2.value=="Select your side"){ // if user does not buy the required pieces , system won't let to proceed.
+        window.location.href="order.html";
+    }else{
+        window.location.href='checkout.html';
+    }
+    
 }
 
 function randomAudio(){/*script to get a random audio on click every time.*/
