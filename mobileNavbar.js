5f869f5e6d9b2329092bb131c880ae0eb047d659
yigitEnes <96189534+ubik69@users.noreply.github.com>
2023-10-11 23:39:32 +0100

    real-time end user resolution check added
    fixed a bug mentioned on previous repositories , about mobile navigation.

--- mobileNavbar.js
+++ mobileNavbar.js
@@ -1,9 +1,17 @@
-function toggleNavbar() {
-  const navbar = document.querySelector('.navbar');
-  
+function toggleNavbar() { /*open and close for mobile navigation */
+  let navbar = document.querySelector('.navbar');
   if (navbar.style.display == "none") {
     navbar.style.display = "block";
-  } else {
+  }else{
     navbar.style.display="none";
   }
 }
+function checkRes(){ /*function to make sure after bigger widths , mobile navigation is hidden. */
+  let navbar = document.querySelector('.navbar');
+  let deviceWidth = window.innerWidth;
+  if(deviceWidth > 630){
+    navbar.style.display="none";
+  }
+}
+
+window.addEventListener('resize', checkRes); /*needed here to uptade end users width real-time.*/
