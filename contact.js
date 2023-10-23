86901b6eea75eff4af7e32c6265067a59979b7f0
yigitEnes <96189534+ubik69@users.noreply.github.com>
2023-10-23 19:50:50 +0100

    fixed contact javascript
    made it more stable and useful.

--- contact.js
+++ contact.js
@@ -1,3 +1,11 @@
-function thanksForFeeedback(){
+let customerMessage = document.getElementById('messageContact');
+let submitButton = document.getElementById('submitContact');
+
+submitButton.addEventListener('click', function(event) {
+  if (customerMessage.value == '') {
+    alert('Please enter a message before submitting.');
+    event.preventDefault(); // prevent the form from submitting on load.
+  } else {
     alert('We got your message, thanks for contacting.');
-}
+  }
+});
